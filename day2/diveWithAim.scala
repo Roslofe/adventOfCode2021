dbf6a5290ae74d26327543738843e1c20563d181
roslofe1 <ella.roslof@aalto.fi>
2021-12-02 19:20:22 +0200

    Added successful part2 for day 2.

--- day2/diveWithAim.scala
+++ day2/diveWithAim.scala
@@ -1,24 +1,29 @@
 package day2
 import scala.io.Source
 
-object dive extends App {
+object diveWithAim extends App {
 
   val file = Source.fromFile("day2/directions.txt")
 
+  /** Functions identically to the dive-object, with
+   * the only exception being the addition of the
+   * aim-variable, and what changes each command does */
   try {
     var horizontal = 0
     var depth = 0
+    var aim = 0
     val directions = file.getLines().toVector.map(_.split(" "))
     for (command <- directions) {
       val goTo = command(0)
       val howMuch = command(1).toInt
       /** Goes through each possible command, and changes the value for said direction accordingly. */
-      if (goTo == "forward")
+      if (goTo == "forward") {
         horizontal += howMuch
-      else if (goTo == "down")
-        depth += howMuch
+        depth += howMuch * aim
+      } else if (goTo == "down")
+        aim += howMuch
       else if (goTo == "up")
-        depth -= howMuch
+        aim -= howMuch
     }
     println(s"Final coordinates:\nDepth = $depth\nHorizontal = $horizontal\nMultiplied: ${depth * horizontal}")
   } finally {
